69611bd56326e6f26a16a0692f0116bbfff931b1
Kyle Chance <kylechance@gmail.com>
2016-11-21 18:58:41 -0500

    added update and delete

--- server/routers/post.router.js
+++ server/routers/post.router.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Post = require('../models/post.model.js');
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
 
 
 router.get('/posts', function(req, res){
@@ -15,7 +19,18 @@ router.get('/posts', function(req, res){
     });
   });
 });
-router.get('/posts/:id', function(req, res){});
+router.get('/posts/:id', function(req, res){
+  Post.find({_id: req.params.id}, function(err, post){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+    return res.status(200).json({
+      posts: posts
+    });
+  });
+});
 
 router.post('/posts', function(req, res){
   var post = new Post(req.body);
@@ -33,8 +48,30 @@ router.post('/posts', function(req, res){
   });
 });
 
-router.put('/posts/:id', function(req, res){});
+router.put('/posts/:id', function(req, res){
+  Post.findOneAndUpdate({_id: req.params.id}, req.body, function(err, oldPost){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+    return res.status(200).json({
+      msg: oldPost
+    });
+  });
+});
 
-router.delete('/posts/:id', function(req, res){});
+router.delete('/posts/:id', function(req, res){
+  Post.findOneAndRemove({_id: req.params.id}, function(err, oldPost){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    }
+    res.status(200).json({
+      msg: oldPost
+    });
+  });
+});
 
 module.exports = router;
